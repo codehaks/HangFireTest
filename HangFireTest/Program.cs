87a4c00a3e1cef9b987b3daee029d8da82756220
Hakim Ghods <ghods.hakim@hotmail.com>
2020-07-19 06:32:43 +0430

    Add project files.

---
+++ HangFireTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using System;
+
+namespace HangFireTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GlobalConfiguration.Configuration
+               .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+               .UseColouredConsoleLogProvider()
+               .UseSimpleAssemblyNameTypeSerializer()
+               .UseRecommendedSerializerSettings()
+               .UseSqlServerStorage(@"Data Source=.\sqlexpress;Initial Catalog=HangFireTestDb;Integrated Security=True", new SqlServerStorageOptions
+               {
+                   CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                   SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                   QueuePollInterval = TimeSpan.Zero,
+                   UseRecommendedIsolationLevel = true,
+                   UsePageLocksOnDequeue = true,
+                   DisableGlobalLocks = true
+               });
+
+            BackgroundJob.Enqueue(() => Console.WriteLine("Hello, world!"));
+            BackgroundJob.Schedule(() =>  Console.WriteLine("Delayed") ,TimeSpan.FromSeconds(10));
+
+            using (var server = new BackgroundJobServer())
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
